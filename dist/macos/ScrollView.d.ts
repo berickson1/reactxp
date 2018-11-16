b2db6a7ce45419cd4d559a86b5d76c34392219a4
Brent Erickson <berickson1@users.noreply.github.com>
2018-11-16 11:25:30 -0800

    Commit dist

---
+++ dist/macos/ScrollView.d.ts
@@ -0,0 +1,16 @@
+/**
+ * ScrollView.tsx
+ *
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT license.
+ *
+ * RN Mac-specific implementation of the cross-platform ScrollView abstraction.
+ */
+import * as React from 'react';
+import * as RN from 'react-native';
+import { ScrollView as ScrollViewBase } from '../native-common/ScrollView';
+export declare class ScrollView extends ScrollViewBase {
+    protected _render(nativeProps: RN.ScrollViewProps & React.Props<RN.ScrollView>): JSX.Element;
+    private _onKeyDown;
+}
+export default ScrollView;
